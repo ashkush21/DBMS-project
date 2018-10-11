586610db3eaa546899051344b17f10a6ad4bec09
shradha <shradhakhapra1899@gmail.com>
2018-10-11 19:05:38 +0530

    4 html files with css added

---
+++ doctors.js
@@ -0,0 +1,14 @@
+var reg=document.getElementById("reg");
+var log=document.getElementById('log');
+var regbox=document.getElementById("regbox");
+var logbox=document.getElementById('logbox');
+reg.addEventListener('click',function(){
+    regbox.style.display="block";
+    reg.style.display="none";
+    log.style.display="none";
+});
+log.addEventListener('click',function(){
+    logbox.style.display="block";
+    reg.style.display="none";
+    log.style.display="none";
+});
